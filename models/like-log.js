cdc81065fc8e01dee3442e942b08142bab161c30
Yuri Lee <leyuri97@gmail.com>
2018-11-10 23:25:26 +0900

    좋아요 db 수정

---
+++ models/like-log.js
@@ -0,0 +1,18 @@
+//좋아요
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var schema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  competition: { type: Schema.Types.ObjectId, ref: 'Event' },
+  createdAt: {type: Date, default: Date.now}
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+var LikeLog = mongoose.model('LikeLog', schema);
+
+module.exports = LikeLog;
+
+
+//왜 반영이 안되는거지
